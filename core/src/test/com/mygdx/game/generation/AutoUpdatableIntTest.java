141244fae5fc9d8e38d34c2de5a8c2ae66609cc2
ivrybachenko <vanya150599@mail.ru>
2021-08-10 10:07:03 +0700

    [REFACTOR] Made tests more atomic

--- core/src/test/com/mygdx/game/generation/AutoUpdatableIntTest.java
+++ core/src/test/com/mygdx/game/generation/AutoUpdatableIntTest.java
@@ -3,6 +3,7 @@ package com.mygdx.game.generation;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.generation.primitive.SequenceIntSupplier;
 import com.mygdx.game.generation.primitive.StaticFloatSupplier;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AutoUpdatableIntTest {
 
     @Test
-    void test1() {
+    void givesTheFirstValueRightAfterCreation() {
         Array<Integer> values = new Array<>();
         values.addAll(1, 2);
         AutoUpdatableInt autoUpdatableInt = new AutoUpdatableInt(new SequenceIntSupplier(values),
@@ -18,12 +19,46 @@ class AutoUpdatableIntTest {
 
         assertThat(autoUpdatableInt.getValue())
                 .isEqualTo(1);
+    }
+
+    @Test
+    void givesTheFirstValueBeforeTimeout() {
+        Array<Integer> values = new Array<>();
+        values.addAll(1, 2);
+        AutoUpdatableInt autoUpdatableInt = new AutoUpdatableInt(new SequenceIntSupplier(values),
+                new StaticFloatSupplier(100));
+
         autoUpdatableInt.commitTime(99);
+
         assertThat(autoUpdatableInt.getValue())
                 .isEqualTo(1);
-        autoUpdatableInt.commitTime(1);
+    }
+
+    @Test
+    void givesTheSecondValueAfterTimeout() {
+        Array<Integer> values = new Array<>();
+        values.addAll(1, 2);
+        AutoUpdatableInt autoUpdatableInt = new AutoUpdatableInt(new SequenceIntSupplier(values),
+                new StaticFloatSupplier(100));
+
+        autoUpdatableInt.commitTime(100);
+
         assertThat(autoUpdatableInt.getValue())
                 .isEqualTo(2);
     }
 
+
+    @Test
+    void sumsCommittedTime() {
+        Array<Integer> values = new Array<>();
+        values.addAll(1, 2);
+        AutoUpdatableInt autoUpdatableInt = new AutoUpdatableInt(new SequenceIntSupplier(values),
+                new StaticFloatSupplier(100));
+
+        autoUpdatableInt.commitTime(99);
+        autoUpdatableInt.commitTime(1);
+
+        assertThat(autoUpdatableInt.getValue())
+                .isEqualTo(2);
+    }
 }
